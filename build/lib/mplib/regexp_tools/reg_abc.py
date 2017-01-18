5baa64f2cd9c98ebc151f9ccdfc2b9cd56b4ab9e
wfyxz <274764441@qq.com>
2017-01-18 16:28:32 +0800

    test程序独立

--- build/lib/mplib/regexp_tools/reg_abc.py
+++ build/lib/mplib/regexp_tools/reg_abc.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 # -*- coding: UTF-8 -*-
-import re
 
 def reg_abc(regexp_para):
     # 赋值
@@ -104,50 +103,3 @@ def reg_abc(regexp_para):
     regexp_out = regexp_out.decode('utf-8')
     return regexp_out
 
-def unit_test(regexp_para,test_case):
-    # 正则实现
-    regexp_out = reg_abc(regexp_para)
-    print regexp_out
-    for line in test_case:
-        sample_text, r = line
-        # print sample_text
-        try:
-            if re.search(regexp_out, sample_text) is None:
-                rr = False
-            else:
-                rr = True
-            if rr != r:
-                print sample_text, r
-        except Exception as e:
-            # print e
-            print "reg_abc怎么还有bug？"
-
-if __name__ == "__main__":
-    # 字典结构：{中心词(值、距离、逻辑)，前缀词(值、距离、逻辑)，后缀词(值、距离、逻辑)}，中心距离只能为空，前后缀值/距离/逻辑应该同时存在或不存在。
-    # 测试文本：[(字符串,Y/N),(字符串,Y/N),(字符串,Y/N)]，对同一个字典做一系列测试，输出匹配错误的case
-    regexp_para = dict(
-        center=dict(
-            value="中心",
-            distance="",
-            logic="包含",
-        ),
-        head=dict(
-            value="前缀|前面",
-            distance="3",
-            logic="不包含",
-        ),
-        tail=dict(
-            value="后缀|后面",
-            distance="2",
-            logic="不包含",
-        ),
-    )
-    test_case = [
-        (u"中心", True),
-        (u"中心后缀", False),
-        (u"前面是是中心的后缀", False),
-        (u"前缀是是是中心不不后缀", True),
-        (u"前缀是是是中心不后缀的", False),
-        (u"前缀是是中心不不后面", False)
-    ]
-    unit_test(regexp_para, test_case)
