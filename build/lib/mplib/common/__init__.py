5f06a85ed553b9867ed56286671ea274f928b7fe
潘俊俊 <keyps2@icloud.com>
2017-01-20 18:47:31 +0800

    generators和trees的调用路径优化

--- build/lib/mplib/common/__init__.py
+++ build/lib/mplib/common/__init__.py
@@ -7,4 +7,6 @@ from base_class import AttributeDict
 # region function import
 from decorators import time_elapse, clock_elapse
 from unicode_tools import to_unicode, change_charset, smart_decode, smart_encode
+from generators import g_fib, g_excel_col_name, g_alphabet
+from trees import tag_to_tree
 # endregion
