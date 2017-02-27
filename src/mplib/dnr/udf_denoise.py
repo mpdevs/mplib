3c630335753b0b71e3423d460deb46a529ac4f2a
潘俊俊 <keyps2@icloud.com>
2017-02-27 13:00:55 +0800

    测试

--- src/mplib/dnr/udf_denoise.py
+++ src/mplib/dnr/udf_denoise.py
@@ -12,33 +12,33 @@ reload(sys)
 sys.setdefaultencoding("utf8")
 
 try:
-    # reader = pandas.read_table(sys.stdin, header=None, sep='\t', quoting=csv.QUOTE_NONE, encoding='utf-8', index_col=None, iterator=True)
-    # while True:
-    #     try:
-    #         chunk = reader.get_chunk(2000)
-    #         dd = DataDenoiser(chunk)
-    #         dd.use_keywords = True
-    #         dd.use_tag = False
-    #         dd.use_length = True
-    #         dd.use_series = False
-    #         dd.use_client = False
-    #         dd.use_special_characters = False
-    #         dd.use_edit_distance = False
-    #         dd.run()
-    #     except StopIteration:
-    #         break
-    data = [line for line in sys.stdin]
-    data = map(lambda x: smart_decode(x).replace("\n", "").replace("\r", "").split("\t"), data)
-    dd = DataDenoiser(data=data, content_index=1, head=["id", "content"])
-    dd.use_keywords = True
-    dd.use_tag = False
-    dd.use_length = True
-    dd.use_series = False
-    dd.use_client = False
-    dd.use_special_characters = False
-    dd.use_edit_distance = False
-    dd.udf_support = True
-    dd.run()
-except Exception as e:
-    print "\t".join([str(e), traceback.format_exc()])
+    reader = pandas.read_table(sys.stdin, header=None, sep='\t', quoting=csv.QUOTE_NONE, encoding='utf-8', index_col=None, iterator=True)
+    while True:
+        try:
+            chunk = reader.get_chunk(2000)
+            dd = DataDenoiser(chunk)
+            dd.use_keywords = True
+            dd.use_tag = False
+            dd.use_length = True
+            dd.use_series = False
+            dd.use_client = False
+            dd.use_special_characters = False
+            dd.use_edit_distance = False
+            dd.run()
+        except StopIteration:
+            break
+#     data = [line for line in sys.stdin]
+#     data = map(lambda x: smart_decode(x).replace("\n", "").replace("\r", "").split("\t"), data)
+#     dd = DataDenoiser(data=data, content_index=1, head=["id", "content"])
+#     dd.use_keywords = True
+#     dd.use_tag = False
+#     dd.use_length = True
+#     dd.use_series = False
+#     dd.use_client = False
+#     dd.use_special_characters = False
+#     dd.use_edit_distance = False
+#     dd.udf_support = True
+#     dd.run()
+# except Exception as e:
+#     print "\t".join([str(e), traceback.format_exc()])
 
