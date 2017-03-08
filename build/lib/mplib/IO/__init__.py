c45ae1b13c4c6ea0e2f8d45e064050ce68193895
wfyxz <274764441@qq.com>
2017-03-08 15:38:43 +0800

    11

--- build/lib/mplib/IO/__init__.py
+++ build/lib/mplib/IO/__init__.py
@@ -1,6 +1,5 @@
 # coding: utf-8
 # __author__: u"John"
-from mplib.IO.db.hive.connector import Hive
 from mplib.IO.db.mysql.connector import MPMySQL
 from mplib.IO.db.postgresql.connector import MPPG
 from mplib.IO.db.redis.connector import MPRedis
