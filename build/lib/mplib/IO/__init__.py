7a92a6162625551e5dbec579a9635d99dbd4f83b
Tsai Tsung Wei <tsungwei_peter@yahoo.com.tw>
2017-04-11 16:37:48 +0800

    保证IO模块不会因为驱动问题影响加载

--- build/lib/mplib/IO/__init__.py
+++ build/lib/mplib/IO/__init__.py
@@ -4,11 +4,32 @@ from __future__ import unicode_literals
 from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
-from mplib.IO.db.hive.connector import Hive
-from mplib.IO.db.mysql.connector import MPMySQL as MySQL
-from mplib.IO.db.postgresql.connector import MPPG as PostgreSQL
-from mplib.IO.db.redis.connector import MPRedis as Redis
-from mplib.IO.db.impala.connector import Impala
-from mplib.IO.db.mysql.connector import db_connect as get_mysql_connect
-from mplib.IO.db.mysql.connector import db_cursor as get_mysql_cursor
+
+try:
+    from mplib.IO.db.hive.connector import Hive
+except ImportError:
+    pass
+
+try:
+    from mplib.IO.db.mysql.connector import MPMySQL as MySQL
+    from mplib.IO.db.mysql.connector import db_connect as get_mysql_connect
+    from mplib.IO.db.mysql.connector import db_cursor as get_mysql_cursor
+except ImportError:
+    pass
+
+try:
+    from mplib.IO.db.postgresql.connector import MPPG as PostgreSQL
+except ImportError:
+    pass
+
+try:
+    from mplib.IO.db.redis.connector import MPRedis as Redis
+except ImportError:
+    pass
+
+try:
+    from mplib.IO.db.impala.connector import Impala
+except ImportError:
+    pass
+
 from mplib.IO.file.binary.pickle_helper import pickle_load, pickle_dump
