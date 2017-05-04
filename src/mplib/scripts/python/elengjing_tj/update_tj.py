2e108ec5f973066feadd82d6db9a36209f58c716
潘俊俊 <keyps2@icloud.com>
2017-05-04 17:48:16 +0800

    添加同位市场的计算脚本

---
+++ src/mplib/scripts/python/elengjing_tj/update_tj.py
@@ -0,0 +1,96 @@
+# coding: utf-8
+# __author__: u"John"
+from __future__ import unicode_literals, absolute_import, print_function, division
+from mplib.IO import PostgreSQL, Hive
+
+
+def get_registered_shop():
+    sql = """
+    SELECT DISTINCT s.id
+    FROM elengjing.public.user AS u
+    JOIN elengjing.public.shop AS s
+    ON u.shop_id = s.id
+    WHERE s.is_validated = 't'
+    """
+    return PostgreSQL(env="v2_uat").query(sql)
+
+
+def etl():
+    shop_ids = get_registered_shop()
+
+    if not shop_ids:
+        return
+
+    sql = """
+    USE elengjing_tj;
+    DROP TABLE IF EXISTS t_shop_{id};
+    CREATE TABLE t_shop_{id} AS
+    SELECT
+        c.categoryid AS category_id,
+        c.categoryname AS category_name,
+        c.shopid AS customer_shop_id,
+        c.shopname AS customer_shop_name,
+        c.itemid AS customer_item_id,
+        c.itemname AS customer_item_name,
+        t.shopid AS target_shop_id,
+        t.shopname AS target_shop_name,
+        t.itemid AS target_item_id,
+        t.itemname AS target_item_name
+    FROM (
+        SELECT
+            shopid,
+            shopname,
+            categoryid,
+            categoryname,
+            itemid,
+            itemname,
+            CASE
+                WHEN NVL(SUM(salesqty), 0) = 0
+                THEN 0
+                ELSE 1.0 * NVL(SUM(salesamt), 0) / SUM(salesqty)
+            END AS avg_price
+        FROM elengjing.women_clothing_item
+        WHERE shopid = {id}
+        GROUP BY
+            shopid,
+            shopname,
+            categoryid,
+            categoryname,
+            itemid,
+            itemname
+    ) AS c
+    CROSS JOIN (
+        SELECT
+            shopid,
+            shopname,
+            categoryid,
+            categoryname,
+            itemid,
+            itemname,
+            CASE
+                WHEN NVL(SUM(salesqty), 0) = 0
+                THEN 0
+                ELSE 1.0 * NVL(SUM(salesamt), 0) / SUM(salesqty)
+            END AS avg_price
+        FROM elengjing.women_clothing_item
+        WHERE shopid != {id}
+        GROUP BY
+            shopid,
+            shopname,
+            categoryid,
+            categoryname,
+            itemid,
+            itemname
+    ) AS t
+    WHERE c.categoryid = t.categoryid
+    AND t.avg_price BETWEEN 0.8 * c.avg_price AND 1.2 *c.avg_price;
+    DROP TABLE IF EXISTS shop_{id};
+    ALTER TABLE t_shop_{id} RENAME TO shop_{id};
+    """
+
+    for shop_id in shop_ids:
+        Hive(env="idc").execute(sql.format(**shop_id))
+
+
+if __name__ == "__main__":
+    etl()
