5a3419bd987529236335bbb354c65164020db6ee
潘俊俊 <keyps2@icloud.com>
2017-05-04 17:47:39 +0800

    调整word vector的提取策略

--- src/mplib/scripts/python/competition/word_vector.py
+++ src/mplib/scripts/python/competition/word_vector.py
@@ -1,47 +1,45 @@
 # coding: utf-8
 # __author__: u"John"
 from __future__ import unicode_literals, absolute_import, print_function, division
+from mplib.common import time_elapse
 from mplib.IO import PostgreSQL
 from io import open
 import pandas
 import json
 
 
+@time_elapse
 def get_attr_pnv():
-    db = PostgreSQL()
     sql = """
     SELECT
-      attr_name, attr_value
+      DISTINCT attr_value
     FROM attr_value
     WHERE industry_id = 16
     AND attr_name <> '品牌'
     """
-    ret = db.query(sql)
-    s = list()
-    for line in ret:
-        s.append(line.get("attr_name"))
-        for attr_value in line.get("attr_value").split(","):
-            s.append(attr_value)
-    s = list(set(s))
-    print("get_attr_pnv.s:", len(s))
-    return s
+    return list(map(lambda x: x.get("attr_value"), PostgreSQL(env="v2_uat").query(sql)))
 
 
+@time_elapse
 def get_word_vec():
     with open("vectors.txt", encoding="utf8") as f:
         d = [line.replace("\r", "").replace("\n", "").split(" ") for line in f]
-    print("get_word_vec.d:", len(d))
     return d
 
 
+@time_elapse
 def process_wv():
-    d = get_word_vec()  # d[0]词, d[1]向量
-    w = list(map(lambda x: x[0], d))  # 所有词
-    s = get_attr_pnv()  # 去重属性值
-    d = filter(lambda x: x[0] in s, d)  # 在属性值中出现的词向量
-    o = filter(lambda x: x in w, s)  # 没有在词向量中出现的属性值
-    print("process_wv.d:", len(d))
+    word_vector = get_word_vec()  # d[0]词, d[1]向量
+    word = list(map(lambda x: x[0], word_vector))  # 所有词
+    print("word_vector length:", len(word))
+    attr_value = get_attr_pnv()  # 去重属性值
+    print("attr_value length:", len(attr_value))
+    d = list(filter(lambda x: x[0] in attr_value, word_vector))  # 在属性值中出现的词向量
+    print("word_vector in attr_value length:", len(d))
+    o = list(filter(lambda x: x not in word, attr_value))  # 没有在词向量中出现的属性值
+    print("attr_value not in word_vector length:", len(o))
     pandas.DataFrame(d, columns=None).to_csv("filtered_vectors.txt", sep=b" ", encoding="utf8", index=False, header=False)
+    print("vector exported")
     pandas.DataFrame(o, columns=None).to_csv("oov.txt", sep=b" ", encoding="utf8", index=False, header=False)
 
 
@@ -84,5 +82,6 @@ def read_from_db():
 if __name__ == "__main__":
     # j = read_from_db()
     # print(len(list(j)))
-    # get_attr_pnv()
+    # print(len(get_attr_pnv()))
     process_wv()
+
