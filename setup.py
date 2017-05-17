4e5e051a6e0627c42516e400c94440b52362fb1a
潘俊俊 <keyps2@icloud.com>
2017-05-17 14:12:12 +0800

    版本更新

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mplib",
-    version="0.5.6",
+    version="0.5.7",
     packages=find_packages("src"),
     package_dir={"": "src"},
     include_package_data=True,
