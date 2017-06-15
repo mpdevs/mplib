2b8ae9af82da4f3d48e11c8366de3ad2d89a1f4f
潘俊俊 <keyps2@icloud.com>
2017-06-16 02:06:40 +0800

    no message

--- src/mplib/scripts/python/pricing/predict.py
+++ src/mplib/scripts/python/pricing/predict.py
@@ -15,6 +15,6 @@ if __name__ == "__main__":
     except:
         is_event = False
     print("{0} 获取品类成功".format(datetime.now()))
-    for category_id in category_list:
+    for idx, category_id in enumerate(category_list):
         print("{0} 正在处理品类 {1} ({2}/{3})".format(datetime.now(), category_id, idx + 1, len(category_list)))
         hive_data_predict(category_id, is_event)
