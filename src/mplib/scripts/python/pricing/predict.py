be34a949292b02a620907db43b677bb4a0ede655
潘俊俊 <keyps2@icloud.com>
2017-06-15 18:41:40 +0800

    增加日志信息

--- src/mplib/scripts/python/pricing/predict.py
+++ src/mplib/scripts/python/pricing/predict.py
@@ -3,14 +3,18 @@
 from __future__ import unicode_literals, absolute_import, print_function, division
 from mplib.pricing.helper import get_all_category
 from mplib.pricing.api import hive_data_predict
+from datetime import datetime
 import sys
 
 
 if __name__ == "__main__":
+    category_list = get_all_category()
     try:
         is_event = sys.argv[1]
         is_event = is_event == "event"
     except:
         is_event = False
-    for category_id in get_all_category():
+    print("{0} 获取品类成功".format(datetime.now()))
+    for category_id in category_list:
+        print("{0} 正在处理品类 {1} ({2}/{3})".format(datetime.now(), category_id, idx + 1, len(category_list)))
         hive_data_predict(category_id, is_event)
