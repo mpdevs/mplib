96772ac5821f08ddca08485ca9ff21b52be5fc77
潘俊俊 <keyps2@icloud.com>
2017-06-15 16:15:06 +0800

    更新价格预测脚本

--- src/mplib/scripts/python/pricing/predict.py
+++ src/mplib/scripts/python/pricing/predict.py
@@ -2,7 +2,10 @@
 # __author__: u"John"
 from __future__ import unicode_literals, absolute_import, print_function, division
 from mplib.pricing.helper import get_all_category
+from mplib.pricing.api import hive_data_predict
 
 
 if __name__ == "__main__":
-    get_all_category()
+    for i in get_all_category():
+        hive_data_predict(i)
+
